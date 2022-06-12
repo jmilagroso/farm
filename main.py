aa1ee6838930e76fae0c679e738bc44680a45339
jmilagroso <j.milagroso@gmail.com>
2022-06-12 16:32:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ lot_area_sqm = df['lot_area_sqm'].drop_duplicates()
 lot_area_sqm = pd.concat([pd.Series(['Filter by lot area']), lot_area_sqm])
 lot_area_sqm_choice = st.sidebar.selectbox('', lot_area_sqm)
 
-st.sidebar.text('**Reservation Fee**: Php 30,000.00\n**Computation**: 70% Loan, 30% DP\n**Inclusions**: 1 BR Nipa Hut,\n5 Goats,\n10 Native Chickens,\n1 Pair Turkey,\n2 Buddah Bamboo Poles')
+st.text('**Reservation Fee**: Php 30,000.00\n**Computation**: 70% Loan, 30% DP\n**Inclusions**: 1 BR Nipa Hut,\n5 Goats,\n10 Native Chickens,\n1 Pair Turkey,\n2 Buddah Bamboo Poles')
 
 st.sidebar.text('**Contact Info**: +639560861684\n+639917285314\n+639760445567')
 
