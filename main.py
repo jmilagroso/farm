facc13180e6547e83f5311953ab98d431ff76e75
jmilagroso <j.milagroso@gmail.com>
2022-06-12 16:28:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sh
 df = pd.read_csv(url)
 
 st.sidebar.text('Reservation Fee : Php 30,000.00\nComputation: 70% Loan, 30% DP\nInclusions: 1 BR Nipa Hut,\n5 Goats,\n10 Native Chickens,\n1 Pair Turkey,\n2 Buddah Bamboo Poles')
-st.sidebar.text('Contact: +639560861684\n+639917285314\n+639760445567')
+
 
 lot_area_sqm = df['lot_area_sqm'].drop_duplicates()
 lot_area_sqm = pd.concat([pd.Series(['All']), lot_area_sqm])
 lot_area_sqm_choice = st.sidebar.selectbox('Lot Area (sqm):', lot_area_sqm)
+
+st.sidebar.text('Contact: +639560861684\n+639917285314\n+639760445567')
 
 if lot_area_sqm_choice != 'All':
   df = df.query(f'lot_area_sqm == {lot_area_sqm_choice}')
