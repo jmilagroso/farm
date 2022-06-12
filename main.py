e4ac61008c6a2a46dd7126f7dfdbd5831d6bc13a
jmilagroso <j.milagroso@gmail.com>
2022-06-12 14:55:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import streamlit as st
 
+sheet_id   = st.secrets["google-spreadsheet"]["sheet_id"]
+sheet_name = st.secrets["google-spreadsheet"]["sheet_name"]
+
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+df = pd.read_csv(url)
+
+st.dataframe(df)
