7d0192166c81c304809fe3992bd4b94cbf5b20d7
jmilagroso <j.milagroso@gmail.com>
2022-06-12 16:38:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ df = pd.read_csv(url)
 
 lot_area_sqm = df['lot_area_sqm'].drop_duplicates()
 lot_area_sqm = pd.concat([pd.Series(['Filter by lot area']), lot_area_sqm])
-lot_area_sqm_choice = st.sidebar.selectbox('', lot_area_sqm)
+lot_area_sqm_choice = st.selectbox('', lot_area_sqm)
 
 st.sidebar.text('Reservation Fee: Php 30,000.00\nComputation: 70% Loan, 30% DP\nInclusions: 1 BR Nipa Hut,\n5 Goats,\n10 Native Chickens,\n1 Pair Turkey,\n2 Buddah Bamboo Poles')
 
